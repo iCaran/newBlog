bc60029b05203a29382c21932e29a84ab5abd1d8
iCaran <pratapmorgan@gmail.com>
2026-02-16 01:05:42 +0530

    Deploy site

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,5 @@
+
+import __ASTRO_IMAGE_IMPORT_Z2q05HY from "./cover.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fabdication_1%2Findex.mdx";
+import __ASTRO_IMAGE_IMPORT_2j0pvA from "./cover.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fabdication_2%2Findex.mdx";
+export default new Map([["./cover.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fabdication_1%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z2q05HY], ["./cover.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fabdication_2%2Findex.mdx", __ASTRO_IMAGE_IMPORT_2j0pvA]]);
+		
---
+++ .astro/content-modules.mjs
@@ -0,0 +1,18 @@
+
+export default new Map([
+["src/content/blog/abdication_1/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fabdication_1%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/blog/abdication_2/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fabdication_2%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/integrations/advanced.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintegrations%2Fadvanced.mdx&astroContentModuleFlag=true")],
+["src/content/docs/integrations/comment.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintegrations%2Fcomment.mdx&astroContentModuleFlag=true")],
+["src/content/docs/integrations/components.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintegrations%2Fcomponents.mdx&astroContentModuleFlag=true")],
+["src/content/docs/integrations/content-present.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintegrations%2Fcontent-present.mdx&astroContentModuleFlag=true")],
+["src/content/docs/integrations/links.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintegrations%2Flinks.mdx&astroContentModuleFlag=true")],
+["src/content/docs/integrations/others.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintegrations%2Fothers.mdx&astroContentModuleFlag=true")],
+["src/content/docs/advanced/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fadvanced%2Fabout.mdx&astroContentModuleFlag=true")],
+["src/content/docs/advanced/customize.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fadvanced%2Fcustomize.mdx&astroContentModuleFlag=true")],
+["src/content/docs/advanced/optimize.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fadvanced%2Foptimize.mdx&astroContentModuleFlag=true")],
+["src/content/docs/advanced/update.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fadvanced%2Fupdate.mdx&astroContentModuleFlag=true")],
+["src/content/docs/setup/content.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsetup%2Fcontent.mdx&astroContentModuleFlag=true")],
+["src/content/docs/setup/deployment.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsetup%2Fdeployment.mdx&astroContentModuleFlag=true")],
+["src/content/docs/setup/getting-started.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsetup%2Fgetting-started.mdx&astroContentModuleFlag=true")]]);
+		
